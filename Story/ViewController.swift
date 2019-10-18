af7a9960e5f886a8b80ac83f142ae2a93dfa82a0
cooper <15204036698@163.com>
2019-10-18 15:55:59 +0800

    完成第二次文件添加的提交

---
+++ Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by apple on 2019/10/18.
+//  Copyright © 2019 HeZhiBo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
